aa0a04eeb514f914a1e6855ec96423a40b7e8af6
cristian-expertbook <cristianabf2002@gmail.com>
2026-09-18 23:52:43 -0300

    refactor: se agrega PlayerBehaviour a Iris con el fin de que el jugador tenga salud, pueda hacer respawn y prepararlo para implementar enemigos.

--- Assets/Character/PlayerBehaviour.cs
+++ Assets/Character/PlayerBehaviour.cs
@@ -14,10 +14,21 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void Update()
     {
+        PlayerDead();
+    }
+
+    private void PlayerDead()
+    {
         if (playerHealth <= 0f)
         {
             isDead = true;
             Debug.Log("You have been killed!");
+
+            playerHealth = 100f;
+            Debug.Log("Salud restaurada");
         }
     }
+
+    public void ResetState() {isDead = false;}
+    public bool GetState() {return isDead;}
 }

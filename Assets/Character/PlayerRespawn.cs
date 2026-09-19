f2c4bc156a71fb3491235bb3d1df0293e4d24932
cristian-expertbook <cristianabf2002@gmail.com>
2026-09-18 23:50:31 -0300

    refactor: se modifica Update para que tome el bool isDead de PlayerBehaviour y lo tenga en cuenta para el respawn.

--- Assets/Character/PlayerRespawn.cs
+++ Assets/Character/PlayerRespawn.cs
@@ -4,28 +4,36 @@ public class PlayerRespawn : MonoBehaviour
 {
     public Transform respawnP;
     private CharacterController controller;
+    PlayerBehaviour playerBehaviour;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        playerBehaviour = GetComponent<PlayerBehaviour>();
     }
 
     void Update()
     {
         if (respawnP == null) return;
 
-        if (transform.position.y < -5.0f)
+        if (transform.position.y < -5.0f || playerBehaviour.GetState() == true)
         {
-            if (controller != null)
-            {
-                controller.enabled = false;
-                transform.position = respawnP.position;
-                controller.enabled = true;
-            }
-            else
-            {
-                transform.position = respawnP.position;
-            }
+            pRespawn();
         }
+    }
+
+    void pRespawn()
+    {
+        if (controller != null)
+        {
+            controller.enabled = false;
+            transform.position = respawnP.position;
+            controller.enabled = true;
+        }
+        else
+        {
+            transform.position = respawnP.position;
+        }
+        playerBehaviour.ResetState();
     }
 }
